4b2909244cd1c62377de043d508f4a3e14aaf414
sitek94 <msitkowski94@gmail.com>
2021-01-25 19:29:36 +0100

    fetch list of videos from YouTubeAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import * as React from 'react';
+import axios from 'axios';
+
 function App() {
-  return <div className="App">App</div>;
+  const [term, setTerm] = React.useState('');
+  const [searchResults, setSearchResults] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          q: term,
+          part: 'snippet',
+          maxResult: 5,
+          type: 'video',
+          key: process.env.REACT_APP_YOUTUBE_KEY,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setSearchResults(response.data.items);
+      });
+  }, [term]);
+
+  return (
+    <div className="App">
+      <div id="search-input">
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.currentTarget.value)}
+        />
+      </div>
+      <pre>{JSON.stringify(searchResults, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App;
