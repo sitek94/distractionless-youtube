07b2ea011a92b9a2577642a1b2350765b87af904
sitek94 <msitkowski94@gmail.com>
2021-01-26 20:15:08 +0100

    refactor: extract <HomeScreen> to separate module

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import youtube from 'api/youtube';
 import styled from '@emotion/styled';
 
-import { Routes, Route, Link, useParams } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import SearchScreen from 'screens/search-screen';
 import PlayerScreen from 'screens/player-screen';
+import HomeScreen from 'screens/home-screen';
 
 function App() {
   return (
@@ -20,26 +20,10 @@ function App() {
       </Nav>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomeScreen />} />
         <Route path="/search" element={<SearchScreen />} />
         <Route path="watch/:videoId" element={<PlayerScreen />} />
       </Routes>
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Homepage</h2>
-      <hr />
-      <a
-        href="https://github.com/sitek94/distractionless-youtube"
-        rel="noreferrer noopener"
-        target="_blank"
-      >
-        Checkout the project on GitHub
-      </a>
     </div>
   );
 }
---
+++ src/screens/home-screen.tsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <hr />
+      <a
+        href="https://github.com/sitek94/distractionless-youtube"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
+        Checkout the project on GitHub
+      </a>
+    </div>
+  );
+}
+
+export default Home;
