5407ae133bc5e267d074e8d19046e6e5c0716e07
sitek94 <msitkowski94@gmail.com>
2021-01-27 22:53:26 +0100

    update <AppBar> layout

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -1,46 +1,70 @@
 import * as React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import MuiAppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { Box } from '@material-ui/core';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  })
-);
+interface AppBarProps {
+  children?: React.ReactNode;
+}
 
-function AppBar() {
-  const classes = useStyles();
-
+function AppBar({ children }: AppBarProps) {
   return (
-    <div className={classes.root}>
-      <MuiAppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title} component="div">
-            YouTube
-          </Typography>
-        </Toolbar>
-      </MuiAppBar>
-    </div>
+    <MuiAppBar position="static">
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        {/* Start */}
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            width: 185,
+          }}
+        >
+          <MenuButton />
+          <AppLogo />
+        </Box>
+
+        {/* Center */}
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          {children}
+        </Box>
+
+        {/* End */}
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            width: 185,
+          }}
+        >
+          Buttons placeholder
+        </Box>
+      </Toolbar>
+    </MuiAppBar>
+  );
+}
+
+function MenuButton() {
+  return (
+    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+      <MenuIcon />
+    </IconButton>
+  );
+}
+
+function AppLogo() {
+  return (
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+      YouTube
+    </Typography>
   );
 }
 
